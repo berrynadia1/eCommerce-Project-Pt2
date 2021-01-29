62ab5e361d7486771a940c69a175299af8ab72f0
berrynadia1 <nadiaoberry@gmail.com>
2021-01-29 13:14:40 -0500

    footer fixed

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,22 +9,18 @@ class Footer extends Component{
     render(){
         return(
             <div className="footer">
-                <img className="bottom-logo"
-                src={iconFinder} alt="logo" />
+                <div className="bottom-logo">
+                <img src={iconFinder} alt="logo"/></div>
+                <p className="copyright">Nadia's ShoeWorld 2020 &copy;</p>
                 <a href ="https://twitter.com/?lang=en" target="_blank" rel="noreferrer"className="socials">
                   <Twitter size={36} color="lightBlue" />
                 </a>
                 <a href ="https://www.facebook.com/" target="_blank" rel="noreferrer" className="socials" >
-                   <Facebook size={36} color="blue" />
-                   
+                   <Facebook size={36} color="blue" />   
                 </a>
                 <a href ="https://www.instagram.com/?hl=en" target="_blank" rel="noreferrer" className="socials">
                    <Instagram size={36} color="purple"/>
                 </a>
-                
-                <p className="copyright">Copyight 2020 &copy;</p>
-                
-
             </div>
 
         )
